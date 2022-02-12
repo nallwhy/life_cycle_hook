70d5459d576f55a10f35e6e79c76cc3d30d08239
Jinkyou Son <nallwhy@gmail.com>
2022-02-13 08:26:01 +0900

    Setup publishing

--- mix.exs
+++ mix.exs
@@ -1,14 +1,20 @@
 defmodule LifeCycleHook.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/nallwhy/life_cycle_hook"
+
   def project do
     [
       app: :life_cycle_hook,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description(),
+      source_url: @source_url
     ]
   end
 
@@ -29,5 +35,18 @@ defmodule LifeCycleHook.MixProject do
       {:jason, "~> 1.0", optional: true},
       {:floki, "~> 0.30.0", only: :test}
     ]
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url},
+      maintainers: ["Jinkyou Son(nallwhy@gmail.com)"],
+      files: ["lib", "mix.exs", "LICENSE", "README.md"]
+    ]
+  end
+
+  defp description() do
+    "A simple hook that logs each life-cycle step of LiveView"
   end
 end
