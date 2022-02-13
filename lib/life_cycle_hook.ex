d8b5685acc652ca8a92fdd138d7b602894027810
Jinkyou Son <nallwhy@gmail.com>
2022-02-13 09:45:41 +0900

    Add handle_params hook to LifeCycleHook

--- lib/life_cycle_hook.ex
+++ lib/life_cycle_hook.ex
@@ -14,6 +14,7 @@ defmodule LifeCycleHook do
     socket =
       socket
       |> attach_mount_hook(module)
+      |> attach_handle_params_hook(module)
 
     {:cont, socket}
   end
@@ -24,10 +25,28 @@ defmodule LifeCycleHook do
     socket
   end
 
+  defp attach_handle_params_hook(socket, module) do
+    socket
+    |> attach_hook(:life_cycle_hook, :handle_params, fn _params, _session, socket ->
+      handle_params_hook(socket, module)
+
+      {:cont, socket}
+    end)
+  end
+
   defp mount_hook(socket, module) do
     case connected?(socket) do
       false -> Logger.debug("#{module} mount/3 with HTTP")
       true -> Logger.debug("#{module} mount/3 with Websocket")
     end
+  end
+
+  defp handle_params_hook(socket, module) do
+    case connected?(socket) do
+      false -> Logger.debug("#{module} handle_params/3 with HTTP")
+      true -> Logger.debug("#{module} handle_params/3 with Websocket")
+    end
+
+    {:cont, socket}
   end
 end
